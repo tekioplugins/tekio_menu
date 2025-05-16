c2167c18ecc7a8b3231516c1c471eabcb9f65491
patikpe <patrickpdev@pm.me>
2025-05-16 13:49:25 +0200

    Refactor TekioImageCarousel to use Ink widget for image display; enhance label positioning and styling for better visibility

--- lib/widgets/buttons/image_carousel.dart
+++ lib/widgets/buttons/image_carousel.dart
@@ -15,6 +15,7 @@ class TekioImageCarousel extends Builder {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(8.0),
               ),
+              padding: EdgeInsets.only(right: 10),
               onTap:
                   (index) => TekioMenuNotifier(
                     navPath: menuButton.buttonItems[index].navPath,
@@ -22,32 +23,66 @@ class TekioImageCarousel extends Builder {
               children:
                   menuButton.buttonItems
                       .map(
-                        (e) => Stack(
-                          alignment: AlignmentDirectional.bottomStart,
-                          children: [
-                            Image(
-                              width: double.infinity,
-                              fit: BoxFit.fill,
+                        (e) => Ink(
+                          key: Key(e.buttonKey ?? UniqueKey().toString()),
+                          height: double.maxFinite,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.0),
+                            image: DecorationImage(
                               image: NetworkImage(e.imageUrl ?? ''),
+                              fit: BoxFit.fill,
                             ),
-                            if (e.label != null)
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  left: 15.0,
-                                  bottom: 15.0,
-                                ),
-                                child: Text(
-                                  e.label ?? '',
-                                  style:
-                                      Theme.of(context)
-                                          .textTheme
-                                          .titleMedium, //TODO: Fix poor text visibility
-                                ),
-                              ),
-                          ],
+                          ),
+                          child:
+                              e.label != null
+                                  ? Align(
+                                    alignment: Alignment.bottomLeft,
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(
+                                        left: 10.0,
+                                        bottom: 10.0,
+                                      ),
+                                      child: Text(
+                                        e.label ?? '',
+                                        style:
+                                            Theme.of(
+                                              context,
+                                            ).textTheme.titleMedium,
+                                      ),
+                                    ),
+                                  )
+                                  : const SizedBox.shrink(),
                         ),
                       )
                       .toList(),
             ),
       );
 }
+
+
+
+
+// Stack(
+//                           alignment: AlignmentDirectional.bottomStart,
+//                           children: [
+//                             Image(
+//                               width: double.infinity,
+//                               fit: BoxFit.fill,
+//                               image: NetworkImage(e.imageUrl ?? ''),
+//                             ),
+//                             if (e.label != null)
+//                               Padding(
+//                                 padding: const EdgeInsets.only(
+//                                   left: 15.0,
+//                                   bottom: 15.0,
+//                                 ),
+//                                 child: Text(
+//                                   e.label ?? '',
+//                                   style:
+//                                       Theme.of(context)
+//                                           .textTheme
+//                                           .titleMedium, //TODO: Fix poor text visibility
+//                                 ),
+//                               ),
+//                           ],
+//                         ),
